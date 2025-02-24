cc2da4815e96226ad2ae760b9103ca55c5726b8e
DaveDushi <david.dusi@hotmail.com>
2025-02-24 15:56:30 +0000

    latest after finals

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,7 @@ use serde_json;
 fn handle_bid_details(details: BidDetails) -> String {
     let bid_details: String = run_zkvm(details);
     // let bid_details_string = serde_json::to_string(&bid_details).unwrap();
-    format!("The bid from the zkvm is, {}", bid_details)
+    format!("{}", bid_details)
 }
 
 
